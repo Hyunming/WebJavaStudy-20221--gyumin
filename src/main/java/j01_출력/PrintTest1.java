be97e8fbdbae61cd97fc6668a0474869d84fa963
kgm0986 <kgm5371@naver.com>
2022-10-26 21:43:26 +0900

    별볼일 없는 수정사항

---
+++ src/main/java/j01_출력/PrintTest1.java
@@ -0,0 +1,26 @@
+package j01_출력;
+
+/**
+ *
+ */
+
+public class PrintTest1 {
+    public static void main(String[] args) {
+        //한 줄 주석
+        /*
+        여러줄 주석
+         */
+        /**
+         * 클래스 똔은 메소드의 역할을 명시 또는 설명하기 위한 주석
+         */
+
+        // 문자열 출력
+        System.out.print("이름: \n");
+        System.out.println("김준일");
+        System.out.print("주소:");
+        System.out.println("부산 어딘가");
+
+        boolean is_username_check_flag = true;
+
+    }
+}
